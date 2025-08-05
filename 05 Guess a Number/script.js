a4260d9d7b61f211b771201bf8c936dc231cadcd
shhiivm <shhiivm@pm.me>
2025-08-05 10:37:52 +0530

    Basic Code

---
+++ 05 Guess a Number/script.js
@@ -0,0 +1,12 @@
+const btn = document.querySelector("button");
+
+const randNum =  Math.floor(Math.random()*100)+1;
+
+const attempts = new Array(10);
+
+
+
+btn.addEventListener("click", (e) => {
+  const getData = parseInt(document.querySelector("input").value);
+  
+});
