dd088d6584a92f588c1629c813fcb7540ebd3c1a
shhiivm <shhiivm@pm.me>
2025-08-05 21:09:22 +0530

    Added image loading feature

--- 04 QR Code Generator/script.js
+++ 04 QR Code Generator/script.js
@@ -1,6 +1,24 @@
 document.querySelector('button').addEventListener("click",(e)=>{
-  const text = document.querySelector('#text').value;
-  document.querySelector('#displayQr').innerHTML = `<img src="https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${text}" alt="QR">`;
+
+  const text = document.querySelector('#text').value.trim();
+  const display = document.querySelector('#displayQr');
+
+  if(!text){
+    alert("Please Enter some text");
+    return;
+  }
+
+  //Adding image loading feature.
+    display.innerHTML = `Generating QR Code....`;
+
+    const img = new Image();
+    img.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${encodeURIComponent(text)}`;
+    img.alt = 'QR Code';
+    img.onload = () => {
+      document.querySelector('#text').value = '';
+      display.innerHTML = '';
+      display.appendChild(img);
+    }
 })
 
 
