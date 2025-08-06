cb73e78f690c383f9de7371180a83be41b75a7b1
shhiivm <shhiivm@pm.me>
2025-08-06 19:43:37 +0530

    clear code

--- 05 Guess a Number/script.js
+++ 05 Guess a Number/script.js
@@ -1,36 +1,91 @@
 const btn = document.querySelector("button");
 
-const randNum =  Math.floor(Math.random()*100)+1;
+let randNum = Math.floor(Math.random() * 100) + 1;
 
-// const attempts = new Array(10);
+const getData = document.querySelector("input");
 
+const prevGuess = document.querySelector("#prev-guess");
 
-let totalAttempts = 10;
-btn.addEventListener("click", (e) => {
+const attemptsLeft = document.querySelector("#attempts");
 
-  
-  const getData = parseInt(document.querySelector("input").value);
+const displayResult = document.querySelector("#res");
 
-  const result = document.querySelector('#res');
-  const attemptsLeft = document.querySelector('#attempts');
-  console.log(randNum);
+const p = document.createElement("p");
 
+let previousGuess = [];
+let totalAttempts = 5;
 
-  if(getData === randNum){
-    result.innerHTML = '<p> Won RRRrrrrr<p>';
-  }else{
-    if(totalAttempts <= 0){
-      return result.innerHTML = 'You Lost the Game Your attempts are over';
+let gamePlay = true;
+
+if (gamePlay) {
+  btn.addEventListener("click", (e) => {
+    e.preventDefault();
+    const guess = parseInt(getData.value);
+    validateGuess(guess);
+  });
+}
+
+function validateGuess(guess) {
+  if (isNaN(guess)) {
+    alert("Please Enter a valid number");
+  } else if (guess < 1) {
+    alert("Number Must be more than 1");
+  } else if (guess > 100) {
+    alert("Number Must be less than 100");
+  } else {
+    previousGuess.push(guess);
+    displayGuess(guess);
+    if (totalAttempts < 1) {
+      displayMessage(`Game Over. Correct Guess is ${randNum}`);
+      endGame();
+    } else {
+      // displayGuess(guess);
+      checkGuess(guess);
     }
-    if(getData > randNum){
-        result.innerHTML = `Wrong Answer. Your Guess was high`;
-
-    }
-    if(getData < randNum){
-      result.innerHTML = `Wrong Answer. Your Guess was less`;
-    }
-    totalAttempts--;
-    attemptsLeft.innerHTML = `Attempts Left : ${totalAttempts}`
   }
-  
-});
+}
+
+function checkGuess(guess) {
+  if (guess > randNum) {
+    displayMessage(`Your Guess is more than actual number`);
+  } else if (guess < randNum) {
+    displayMessage(`Your Guess is less than actual number`);
+  } else {
+    displayMessage(`You Got the number it's ${guess}`);
+  }
+}
+
+function displayGuess(guess) {
+  getData.value = "";
+  prevGuess.innerHTML += `${guess}, `;
+  totalAttempts--;
+  attemptsLeft.innerHTML = `Remaining Attempts = ${totalAttempts}`;
+}
+
+function displayMessage(msg) {
+  displayResult.innerHTML = `<h3> ${msg} </h3>`;
+}
+
+function endGame() {
+  getData.innerHTML = "";
+  getData.setAttribute("disabled", "");
+  p.classList.add("button");
+  p.innerHTML = `<button id = "playAgain">Play Again</button>`;
+  displayResult.appendChild(p);
+  gamePlay = false;
+  newGame();
+}
+
+function newGame() {
+  let newGameBtn = document.querySelector('#playAgain');
+  newGameBtn.addEventListener('click', (e) => {
+    
+    randNum = Math.floor(Math.random() * 100) + 1;
+    prevGuess = [];
+    totalAttempts = 5;
+    attemptsLeft.innerHTML = `Remaining Attempts = ${totalAttempts}`;
+    getData.removeAttribute("disabled");
+    displayResult.removeChild(p);
+    gamePlay = true;
+  });
+}
