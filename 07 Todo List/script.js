8f22ef54b6d6c28ea92f9b2d0d26af2bc7881358
shhiivm <shhiivm@pm.me>
2025-08-07 20:59:30 +0530

    initial version completed

--- 07 Todo List/script.js
+++ 07 Todo List/script.js
@@ -1,19 +1,19 @@
 let arr = [
-  {
-    name:'watch movie',
-    dueDate : '25-12-2025'
-  },
-  {
-    name:'play games',
-    dueDate : '20-12-2040'
-  }
+  // {
+  //   name:'watch movie',
+  //   dueDate : '25-12-2025'
+  // },
+  // {
+  //   name:'play games',
+  //   dueDate : '20-12-2040'
+  // }
 ];
 
 const get_btn = document.querySelector("#btn");
 
 const textValue = document.querySelector("#task");
 
-const getDate = document.querySelector('#date');
+const getDate = document.querySelector("#date");
 
 get_btn.addEventListener("click", () => {
   addtodo();
@@ -23,14 +23,23 @@ function addtodo() {
   const name = textValue.value;
   const date = getDate.value;
 
+  if (name === "") {
+    alert("Enter Some task");
+    return;
+  }
+  if (date === "") {
+    alert("Enter date");
+    return;
+  }
+
   arr.push({
-    name:name,
-    dueDate:date
+    name: name,
+    dueDate: date,
   });
   console.log(arr);
 
   textValue.value = "";
-  getDate.value = '';
+  getDate.value = "";
   displayItems();
 }
 
@@ -41,16 +50,21 @@ function displayItems() {
     const todoObject = arr[i];
     const name = todoObject.name;
     const dueDate = todoObject.dueDate;
-    const html = 
-      `<p>
-        ${name} ${dueDate} 
-        <button onclick = "
-          arr.splice(${i},1);
-          displayItems();
-        "> Delete</button>
-      </p>`;
+    const html = `<div class = "add-task">
+        ${name}
+      </div>
+      <div class = "add-date"> 
+        ${dueDate}
+      </div> 
+      <button onclick = "
+        arr.splice(${i},1);
+        displayItems();
+        "
+        class= "delete-btn"
+        > Delete</button>
+        `;
     todoHtml += html;
   }
-  console.log(todoHtml);
+  // console.log(todoHtml);
   document.querySelector("#display-items").innerHTML = todoHtml;
 }
