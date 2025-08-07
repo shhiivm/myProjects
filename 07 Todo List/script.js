58f03f8980c34e4e5700ae9cec1483693cc4d78e
shhiivm <shhiivm@pm.me>
2025-08-07 18:23:38 +0530

    todo js done

---
+++ 07 Todo List/script.js
@@ -0,0 +1,56 @@
+let arr = [
+  {
+    name:'watch movie',
+    dueDate : '25-12-2025'
+  },
+  {
+    name:'play games',
+    dueDate : '20-12-2040'
+  }
+];
+
+const get_btn = document.querySelector("#btn");
+
+const textValue = document.querySelector("#task");
+
+const getDate = document.querySelector('#date');
+
+get_btn.addEventListener("click", () => {
+  addtodo();
+});
+
+function addtodo() {
+  const name = textValue.value;
+  const date = getDate.value;
+
+  arr.push({
+    name:name,
+    dueDate:date
+  });
+  console.log(arr);
+
+  textValue.value = "";
+  getDate.value = '';
+  displayItems();
+}
+
+function displayItems() {
+  let todoHtml = "";
+
+  for (i = 0; i < arr.length; i++) {
+    const todoObject = arr[i];
+    const name = todoObject.name;
+    const dueDate = todoObject.dueDate;
+    const html = 
+      `<p>
+        ${name} ${dueDate} 
+        <button onclick = "
+          arr.splice(${i},1);
+          displayItems();
+        "> Delete</button>
+      </p>`;
+    todoHtml += html;
+  }
+  console.log(todoHtml);
+  document.querySelector("#display-items").innerHTML = todoHtml;
+}
