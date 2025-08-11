c3d44c49fb208e58d6202b302981631903c90bfb
shhiivm <shhiivm@pm.me>
2025-08-11 11:19:39 +0530

    js code changes

--- 09 Random Password Generator/script.js
+++ 09 Random Password Generator/script.js
@@ -7,28 +7,43 @@ const small = caps.toLowerCase();
 const special = "!@#$%^&*()-_=+|[]{};:/?.>";
 const nums = "0123456789";
 
-
-
 btn.addEventListener("click", (e) => {
   e.preventDefault();
 
-  let password = "";
-
   let getLength = document.querySelector("#length");
   let passLength = parseInt(getLength.value);
-  
-  
-  console.log("hello" + passLength);
+
+  let allChars = caps + small + nums + special;
+
+  let password = "";
+  if (passLength < 8) {
+    alert("Password length must be greater than 8");
+    document.querySelector("#length").value = "";
+    return;
+  }
+  if (isNaN(passLength)) {
+    alert(`Enter Valid input`);
+    return;
+  }
+  password += caps.charAt(Math.floor(Math.random() * caps.length));
+  password += small.charAt(Math.floor(Math.random() * small.length));
+  password += nums.charAt(Math.floor(Math.random() * nums.length));
+  password += special.charAt(Math.floor(Math.random() * special.length));
+
   while (password.length < passLength) {
-    password += caps.charAt(Math.floor(Math.random() * caps.length));
-    password += small.charAt(Math.floor(Math.random() * caps.length));
-    password += nums.charAt(Math.floor(Math.random() * nums.length));
-    password += special.charAt(Math.floor(Math.random() * special.length));
+    password += allChars.charAt(Math.floor(Math.random() * allChars.length));
   }
 
   generated_password.value = password;
+  document.querySelector("#length").value = "";
+
+  copyPassword(password);
+  
 });
 
-const arr = new Uint8Array(6);
 
-console.log(crypto.getRandomValues(arr));
+function copyPassword(pass){
+  document.querySelector('#copy').addEventListener('click', ()=>{
+     navigator.clipboard.writeText(pass)
+  })
+}
